5258d6d2edeb533278ba4fba66f5ddb02e5dae2e
Mesbah Uddin <msbh214@gmail.com>
2025-02-14 00:56:45 +0600

    feat: added models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        "Category", on_delete=models.DO_NOTHING, null=False, default=1)
+
+
+class Participant(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    event = models.ManyToManyField(Event)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
