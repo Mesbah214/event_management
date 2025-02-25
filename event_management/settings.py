2f004c92dffa541606ad5f34b2eaacd2476185b5
Mesbah Uddin <msbh214@gmail.com>
2025-02-26 00:40:03 +0600

    chore: initial release version

--- event_management/settings.py
+++ event_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-i43p*bh6+^b42y^=2*xwan_68x_6z(oysi#+#!ay1!&l%w!qdg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
